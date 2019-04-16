5c1f68e26e71530e7d9250c7ee2be3b57dfd43be
Artem Golovinsky <artemgolovinsky@gmail.com>
2019-04-16 22:39:05 +0300

    add pid

--- src/main.rs
+++ src/main.rs
@@ -107,6 +107,7 @@ fn parse(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> Par
         101 => reference_ext(data_stream, make_str, result),
         102 => port_ext(data_stream, make_str, result),
         82 => atom_cache_ref(data_stream, make_str, result),
+        103 => pid_ext(data_stream, make_str, result),
         _ => Err(ParseError::new(ErrorCode::NotImplemented)),
     }
 }
@@ -308,6 +309,19 @@ fn port_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> 
     make_str.make_str_term("port", &res, result);
     Ok(())
 }
+fn pid_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
+    let mut node: String = String::new();
+    parse(data_stream, make_str, &mut node)?;
+    let res: String = format!(
+        "{{\"node\":{},\"id\":{},\"serial\":{},\"creation\":{}}}", 
+        node, 
+        read_u32(data_stream)?, 
+        read_u32(data_stream)?,
+        read_u8(data_stream)?
+    );
+    make_str.make_str_term("pid", &res, result);
+    Ok(())
+}
 
 fn atom_cache_ref(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
     let acr = read_u8(data_stream)?;
@@ -323,7 +337,7 @@ fn bit_binary_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut Strin
 */
 
 fn main() {
-    let mut f = open_file(&"utf.bin".to_string());
+    let mut f = open_file(&"ee.bin".to_string());
     match start_parsing(&mut f) {
         Ok(json) => println!("{}", json),
         Err(error) => println!("Error: {}", error),
