33658f87f11911235195c0348ee33e10fab99d0e
Artem Golovinsky <artemgolovinsky@gmail.com>
2019-04-23 00:24:23 +0300

    add big int

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::Read;
 use std::fmt;
 use std::str;
 use std::io;
+use num_bigint::Sign;
+use num_bigint::BigInt;
 
 
 
@@ -91,11 +93,11 @@ fn parse(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> Par
     let mut el_type: [u8; 1] = [0];
     data_stream.read_exact(&mut el_type)?;
     match el_type[0] {
-        108 => elist_parse(data_stream, make_str, result),
-        107 => estrext_parse(data_stream, make_str, result),
-        98 => einteger_parse(data_stream, make_str, result),
-        97 => esmall_integer_parse(data_stream, make_str, result),
-        100 => eatom_ext_parse(data_stream, make_str, result),
+        108 => list_ext(data_stream, make_str, result),
+        107 => string_ext(data_stream, make_str, result),
+        98 => integer_ext(data_stream, make_str, result),
+        97 => small_integer_ext(data_stream, make_str, result),
+        100 => atom_ext(data_stream, make_str, result),
         115 => small_atom_ext(data_stream, make_str, result),
         104 => small_tuple_ext(data_stream, make_str, result),
         105 => large_tuple_ext(data_stream, make_str, result),
@@ -111,22 +113,17 @@ fn parse(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> Par
         116 => map_ext(data_stream, make_str, result),
         117 => fun_ext(data_stream, make_str, result),
         110 => small_big_ext(data_stream, make_str, result),
+        111 => large_big_ext(data_stream, make_str, result),
        // 112 => new_fun_ext(data_stream, make_str, result),
         _ => Err(ParseError::new(ErrorCode::NotImplemented)),
     }
 }
 
-fn elist_parse(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
-    let l = read_u32(data_stream)?;
+fn list_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
+    let l = read_u32(data_stream)?;    
     let mut list_str: String = String::new();
-    list_str.push_str("[");
-    for n in 0..l {
-        parse(data_stream, make_str, &mut list_str)?;
-        if n + 1 < l {
-            list_str.push_str(",");
-        }
-    };
-    list_str.push_str("]");
+    let mut f = |x: &mut String| parse(data_stream, make_str, x);
+    create_list(l, &mut list_str, &mut f)?;
     if read_u8(data_stream)? == 106 {
         make_str.make_str_term("l", &list_str, result);
         Ok(())
@@ -135,54 +132,42 @@ fn elist_parse(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) 
     }
 }
 
-
-
-fn einteger_parse(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
-    let s = [
-        "\"".to_string(),
-        read_i32(data_stream)?.to_string(),
-        "\"".to_string(),
-    ].concat();
-    make_str.make_str_term("i", &s, result);
+fn integer_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
+    let s = read_i32(data_stream)?.to_string();
+    make_str.make_str_term("i32", &s, result);
     Ok(())
 }
 
-fn esmall_integer_parse(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
-    let s = ["\"".to_string(), read_u8(data_stream)?.to_string(), "\"".to_string()].concat();
-    make_str.make_str_term("i", &s, result);
+fn small_integer_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
+    let s = read_u8(data_stream)?.to_string();
+    make_str.make_str_term("i32", &s, result);
     Ok(())
 }
 
-fn estrext_parse(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
+fn string_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
     let l = read_u16(data_stream)?;
     let mut str_term: String = String::new();
-    str_term.push_str("[");
-    for n in 0..l {
-        str_term.push_str(&(read_u8(data_stream)?).to_string());
-        if n + 1 < l {
-            str_term.push_str(",");
-        }
+    let mut f = |x: &mut String| -> ParseResult {
+        x.push_str(&(read_u8(data_stream)?).to_string());
+        Ok(())
     };
-    str_term.push_str("]");
+    create_list(l as u32, &mut str_term, &mut f)?;
     make_str.make_str_term("str", &str_term, result);
     Ok(())
 }
-fn eatom_ext_parse(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
-    let l = read_u16(data_stream)?;
-    let mut atom_term: String = String::new();
-    atom_term.push_str("\"");
-    for _ in 0..l {
-        atom_term.push(read_u8(data_stream)? as char);
-    };
-    atom_term.push_str("\"");
-    make_str.make_str_term("a", &atom_term, result);
-    Ok(())
+fn atom_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
+    let n = read_u16(data_stream)?;
+    deprecated_atom(n, data_stream, make_str, result)
 }
 fn small_atom_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
-    let l = read_u8(data_stream)?;
+    let n = read_u8(data_stream)?;
+    deprecated_atom(n as u16, data_stream, make_str, result)
+}
+
+fn deprecated_atom(n: u16, data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
     let mut atom_term: String = String::new();
     atom_term.push_str("\"");
-    for _ in 0..l {
+    for _ in 0..n {
         atom_term.push(read_u8(data_stream)? as char);
     };
     atom_term.push_str("\"");
@@ -190,40 +175,20 @@ fn small_atom_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut Strin
     Ok(())
 }
 
-
-
 fn small_tuple_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
-    let l = read_u8(data_stream)?;
-    let mut tuple_str: String = String::new();
-    tuple_str.push_str("{");
-    for n in 0..l {
-        tuple_str.push_str("\"");
-        tuple_str.push_str(&(n + 1).to_string());
-        tuple_str.push_str("\":");
-        parse(data_stream, make_str, &mut tuple_str)?;
-        if n + 1 < l {
-            tuple_str.push_str(",");
-        }
-    };
-    tuple_str.push_str("}");
-    make_str.make_str_term("t", &tuple_str, result);
-    Ok(()) 
+    let n = read_u8(data_stream)?;
+    tuple(n as u32, data_stream, make_str, result)
 }
 
 fn large_tuple_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
-    let l = read_u32(data_stream)?;
+    let n = read_u32(data_stream)?;
+    tuple(n, data_stream, make_str, result)
+}
+
+fn tuple(n: u32, data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
     let mut tuple_str: String = String::new();
-    tuple_str.push_str("{");
-    for n in 0..l {
-        tuple_str.push_str("\"");
-        tuple_str.push_str(&(n + 1).to_string());
-        tuple_str.push_str("\":");
-        parse(data_stream, make_str, &mut tuple_str)?;
-        if n + 1 < l {
-            tuple_str.push_str(",");
-        }
-    };
-    tuple_str.push_str("}");
+    let mut f = |x: &mut String| parse(data_stream, make_str, x);
+    create_list(n, &mut tuple_str, &mut f)?;
     make_str.make_str_term("t", &tuple_str, result);
     Ok(()) 
 }
@@ -257,22 +222,14 @@ fn binary_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -
 }
 fn small_atom_utf8_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
     let len = read_u8(data_stream)?;
-    let mut v: Vec<u8> = vec![];
-    for _ in 0..len {
-        let mut one_b: [u8; 1] = [0];
-        data_stream.read_exact(&mut one_b)?;
-        v.push(one_b[0]);
-    };
-    let s = [
-        "\"".to_string(),
-        str::from_utf8(&v)?.to_string(),
-        "\"".to_string(),
-    ].concat();
-    make_str.make_str_term("a", &s, result);
-    Ok(())
+    atom_utf8(len as u16, data_stream, make_str, result)
 }
 fn atom_utf8_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
     let len = read_u16(data_stream)?;
+    atom_utf8(len, data_stream, make_str, result)
+}
+
+fn atom_utf8(len: u16, data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
     let mut v: Vec<u8> = vec![];
     for _ in 0..len {
         let mut one_b: [u8; 1] = [0];
@@ -336,18 +293,15 @@ fn atom_cache_ref(data_stream: &mut Read, make_str: &MakeStr, result: &mut Strin
 fn map_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
     let arity = read_u32(data_stream)?;
     let mut map_str: String = String::new();
-    map_str.push_str("[");
-    for n in 0..arity {
-        map_str.push_str("{\"key\":");
-        parse(data_stream, make_str, &mut map_str)?;
-        map_str.push_str(",\"val\":");
-        parse(data_stream, make_str, &mut map_str)?;
-        map_str.push_str("}");
-        if n + 1 < arity {
-            map_str.push_str(",");
-        }
+    let mut f = |x: &mut String| -> ParseResult {
+        x.push_str("{\"key\":");
+        parse(data_stream, make_str, x)?;
+        x.push_str(",\"val\":");
+        parse(data_stream, make_str, x)?;
+        x.push_str("}");
+        Ok(())
     };
-    map_str.push_str("]");
+    create_list(arity as u32, &mut map_str, &mut f)?;
     make_str.make_str_term("m", &map_str.to_string(), result);
     Ok(())
 }
@@ -364,15 +318,8 @@ fn fun_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> P
     let mut uniq = String::new();
     parse(data_stream, make_str, &mut uniq)?;
     let mut free_vars = String::new();
-    free_vars.push_str("[");
-    for n in 0..num_free {
-        parse(data_stream, make_str, &mut free_vars)?;
-        if n + 1 < num_free {
-            free_vars.push_str(",");
-        }
-    };
-    free_vars.push_str("]");
-
+    let mut f = |x: &mut String| parse(data_stream, make_str, x);
+    create_list(num_free as u32, &mut free_vars, &mut f)?;
     let res: String = format!(
         "{{\"pid\":{},\"mod\":{},\"index\":{},\"uniq\":{},\"vars\":{}}}", 
         pid, module,  index, uniq, free_vars
@@ -383,21 +330,36 @@ fn fun_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> P
 
 fn small_big_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
     let n = read_u8(data_stream)?;
-    let neg: bool = read_u8(data_stream)? > 0;
-    let mut bytes = String::new();
-    bytes.push_str("[");
+    big(n as u32, data_stream, make_str, result)
+}
+
+fn large_big_ext(data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
+    let n = read_u32(data_stream)?;
+    big(n, data_stream, make_str, result)
+}
+
+fn big(n: u32, data_stream: &mut Read, make_str: &MakeStr, result: &mut String) -> ParseResult {
+    let sign: Sign = if read_u8(data_stream)? > 0 { Sign::Minus } else { Sign::Plus};
+    let mut digits: Vec<u8> = vec![];
+    for _ in 0..n {
+        digits.push(read_u8(data_stream)?);
+    };
+    let r = BigInt::from_bytes_le(sign, &digits);
+    make_str.make_str_term("bi", &r.to_string(), result);
+    Ok(())
+}
+
+
+fn create_list<F>(n: u32, result: &mut String, lf: &mut F) -> ParseResult 
+    where F: FnMut(&mut String) -> ParseResult {
+    result.push_str("[");
     for i in 0..n {
-        bytes.push_str(&read_u8(data_stream)?.to_string());
+        lf(result)?;
         if i + 1 < n {
-            bytes.push_str(",");
+            result.push_str(",");
         }
     };
-    bytes.push_str("]");
-    let res: String = format!(
-        "{{\"neg\":{},\"b\":256,\"d\":{}}}", 
-        neg, bytes
-    );
-    make_str.make_str_term("bi", &res.to_string(), result);
+    result.push_str("]");
     Ok(())
 }
 
